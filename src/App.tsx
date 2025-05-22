b602c7c5d635bb894a48cf28397861b5b1777532
Juan Gabriel Losada <juangabriellosada@hotmail.com>
2025-05-22 16:36:59 -0300

    style(slideComponent): Style slide components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import Header from "./components/header/Header";
 import styled from "styled-components";
+import SlideComponent from "./components/slide-component/slideComponent";
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 1500px;
   display: flex;
+  flex-direction: column;
 `;
 
 function App() {
   return (
     <AppContainer>
       <Header />
+      <SlideComponent slideDirection="right" />
+      <SlideComponent slideDirection="left" />
     </AppContainer>
   );
 }
